2add2de0377721b5488f8e58c412f0802050a57a
Hoang Loc <hoangphuocloc.phurieng@gmail.com>
2024-08-08 08:04:26 +0700

    update code

--- web/controllers/error.js
+++
@@ -1,6 +0,0 @@
-exports.page404 = (req, res, next) => {
-  res.status(404).render('404.ejs', {
-    pageTitle: 'Page not found',
-    path: '/404',
-  });
-};
--- web/util/formatPrice.js
+++
@@ -1,7 +0,0 @@
-function formatCurrency(amount) {
-  if (isNaN(amount)) return amount;
-  let amountStr = amount.toString();
-  return amountStr.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-}
-
-module.exports = { formatCurrency };
